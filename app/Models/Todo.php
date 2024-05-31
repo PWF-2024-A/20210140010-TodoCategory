3240545856cf43705b29f39ceb3849128730d89c
Nadiv Nugraha <nadiv.n.ft21@mail.umy.ac.id>
2024-05-31 14:53:58 +0700

    api : auth, read, search data

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -8,16 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     use HasFactory;
+
+    protected $casts = [
+        'is_complete' => 'boolean',
+    ];
+
     protected $fillable = [
         'title',
         'user_id',
         'category_id',
         'is_complete',
     ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
     }
+
     public function category()
     {
         return $this->belongsTo(Category::class);
---
+++ tugas11.md
@@ -0,0 +1,16 @@
+# Tugas 11
+
+## 1. Login
+![Alt text](screenshot/tugas11/1login.png)
+## 2. Profile
+![Alt text](screenshot/tugas11/2Profile.png)
+## 3. Todo Index
+![Alt text](screenshot/tugas11/3TodoIndex.png)
+## 4. Todo Show
+![Alt text](screenshot/tugas11/4TodoShow.png)
+## 5. Todo Show Forbidden
+![Alt text](screenshot/tugas11/5TodoShowForbidden.png)
+## 6. Todo Search
+![Alt text](screenshot/tugas11/6TodoSearch.png)
+## 7. Logout
+![Alt text](screenshot/tugas11/7Logout.png)
