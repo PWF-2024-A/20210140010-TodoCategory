bb13b72cfd48913173f011282802609b58dc7fda
Nadiv Nugraha <nadiv.n.ft21@mail.umy.ac.id>
2024-06-10 10:32:31 +0700

    api : create, update, delete data

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\ProfileController;
 use App\Http\Controllers\API\TodoController;
 use Illuminate\Support\Facades\Route;
@@ -21,4 +22,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
     Route::apiResource('/todo', TodoController::class);
+    // Route::apiResource('/category', CategoryController::class);
+    Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+    Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
+    Route::patch('/todo/{todo}/uncomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
+    Route::delete('/todo', [TodoController::class, 'deleteAllCompleted'])->name('todo.deleteallcompleted');
 });
---
+++ tugas12.md
@@ -0,0 +1,33 @@
+# Tugas 12
+
+## 1. Category Index
+
+![alt text](screenshot/tugas12/CategoryIndex.png)
+
+## 2. Todo Store
+
+![alt text](screenshot/tugas12/TodoStore.png)
+
+## 3. Todo Store Error
+
+![alt text](screenshot/tugas12/TodoStoreError.png)
+
+## 4. Todo Update
+
+![alt text](screenshot/tugas12/TodoUpdate.png)
+
+## 5. Todo Complete
+
+![alt text](screenshot/tugas12/TodoComplete.png)
+
+## 6. Todo Uncomplete
+
+![alt text](screenshot/tugas12/TodoUncomplete.png)
+
+## 7. Todo Delete
+
+![alt text](screenshot/tugas12/TodoDelete.png)
+
+## 8. Todo Delete All Completed
+
+![alt text](screenshot/tugas12/TodoDeleteAllComplete.png)
